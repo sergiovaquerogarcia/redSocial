d709a5a6e264929b3c307aadef1f04410ce2c6bb
Sergio Vaquero Garcia <svg26@msn.com>
2021-04-28 14:39:03 +0200

    CREO LAS PÁGINAS USANDO TEMPLATES. CREO EL SNIPPET PARA TRABAJAR CON ÉL Y MODIFICO EL FICHERO SERVER PARA USAR LOS TEMPLATES

--- server.py
+++ server.py
@@ -4,25 +4,30 @@
 from flask import Flask, request
 app = Flask(__name__)
 
+from flask import render_template
 
 @app.route('/', methods=['GET'])
 def index():
-    return app.send_static_file('index.html')
+    return render_template("index.html")
+    
 
 
 @app.route('/home', methods=['GET'])
 def home():
-    return app.send_static_file('home.html')
+    return render_template("home.html")
+    
 
 
 @app.route('/login', methods=['GET'])
 def login():
-    return app.send_static_file('login.html')
+    return render_template("login.html")
+    
 
 
 @app.route('/signup', methods=['GET'])
 def signup():
-    return app.send_static_file('signup.html')
+    return render_template("signup.html")
+   
 
 
 @app.route('/processLogin', methods=['GET', 'POST'])
